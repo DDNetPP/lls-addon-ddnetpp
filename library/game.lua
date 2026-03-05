09614cc1a5ae3790dd43d4464e75496fe7af1353
ChillerDragon <chillerdragon@gmail.com>
2026-03-05 14:15:15 +0100

    idk how to write long param texts aaa
    CC https://github.com/LuaLS/lua-language-server/issues/3366

--- library/game.lua
+++ library/game.lua
@@ -12,3 +12,27 @@ function Game:send_chat(message) end
 ---@param client_id integer the client id of the player to find
 ---@return Player|nil player
 function Game:get_player(client_id) end
+
+---@param name string rcon command name
+---@param parameters string teeworlds console parameter types and names
+---                         there are 3 supported types:
+---                         - "i" integer
+---                         - "s" string
+---                         - "r" rest (as string)
+---
+---                         So for example the params "sss" means that the rcon command
+---                         takes 3 strings as parameters.
+---                         And the params "ii" means it takes two integers.
+---                         You can also name the params for example "s[name]i[seconds]"
+---                         it will then pass a table to the callback in the args parameter
+---                         with the keys "name" and "seconds" and the values will be a string and a integer.
+---                         There are also optional parameters which are prefixed with a "?"
+---                         So for example "s[name] ?i[seconds]"
+---                         in that case the command will be executed if one or two arguments are provided
+---                         by the user in the rcon command.
+---                         If non optional parameters were not provided as arguments by the user
+---                         or more arguments were provided than parameters the callback will not be run
+---                         and an error is shown to the user.
+---@param helptext string a short description of the rcon command will be shown in the console next to the completion
+---@param callback fun(client_id: integer, args: table<string, string|integer|nil>) the callback that will be run if a user typed the command name into the remote console
+function Game:register_rcon(name, parameters, helptext, callback) end
