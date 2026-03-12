cef0d74de9f3522833409cc4fc92727018576e62
ChillerDragon <chillerdragon@gmail.com>
2026-03-12 10:48:40 +0100

    Support spaces and other special chars in parameter names

--- spec/args_test.lua
+++ spec/args_test.lua
@@ -2,11 +2,34 @@ local assert_eq = require("simple.assert").assert_eq
 local params_parser = require("../src/params_type")
 local params_to_class = params_parser.params_to_class
 local params_to_cast = params_parser.params_to_cast
+local escape_key_type = params_parser.escape_key_type
 
 
 local function trim(s)
 	return s:match( "^%s*(.-)%s*$" )
 end
+
+assert_eq("x", escape_key_type("x"))
+assert_eq("xx", escape_key_type("xx"))
+assert_eq("x1", escape_key_type("x1"))
+assert_eq(trim([[
+["x x"]
+]]), escape_key_type("x x"))
+assert_eq(trim([[
+["2"]
+]]), escape_key_type("2"))
+assert_eq(trim([[
+["2a"]
+]]), escape_key_type("2a"))
+assert_eq(trim([[
+["²²²"]
+]]), escape_key_type("²²²"))
+assert_eq(trim([[
+["'on'|'off'"]
+]]), escape_key_type("'on'|'off'"))
+assert_eq(trim([[
+["\""]
+]]), escape_key_type([["]]))
 
 assert_eq(trim([[
 ---@class Args
@@ -37,4 +60,10 @@ assert_eq(
 assert_eq(
 	nil,
 	params_to_cast("s[[")
+)
+
+-- https://github.com/LuaLS/lua-language-server/discussions/3378
+assert_eq(
+	[[---@cast args { ["player name"]: string }]],
+	params_to_cast("s[player name]")
 )
--- src/params_type.lua
+++ src/params_type.lua
@@ -1,5 +1,24 @@
 local tw_console = require("src.external.luluworlds.params")
 
+local function escape_for_lua_string(s)
+	return s:gsub("\\", "\\\\")
+		:gsub('"', '\\"')
+		:gsub("\n", "\\n")
+		:gsub("\t", "\\t")
+		:gsub("\r", "\\r")
+		:gsub("\a", "\\a")
+		:gsub("\b", "\\b")
+		:gsub("\v", "\\v")
+		:gsub("\f", "\\f")
+end
+
+local function escape_key_type(name)
+	if name:match("^[%a_][%w_]*$") then
+		return name
+	end
+	return "[\"" .. escape_for_lua_string(name) .. "\"]"
+end
+
 ---@param params_text string # teeworlds params like "i[client_id]"
 ---@return nil|string comment # LuaCATS annotations for the params table
 local function params_to_class(params_text)
@@ -18,7 +37,7 @@ local function params_to_class(params_text)
 	for _, param in ipairs(params) do
 		res =
 			res .. "\n---@field " ..
-			param.name .. " " ..
+			escape_key_type(param.name) .. " " ..
 			type_map[param.type]
 	end
 	return res
@@ -49,7 +68,7 @@ local function params_to_cast(params_text)
 			end
 			res =
 				res .. " " ..
-				param.name .. ": " ..
+				escape_key_type(param.name) .. ": " ..
 				type_map[param.type]
 		end
 	end
@@ -58,6 +77,7 @@ local function params_to_cast(params_text)
 end
 
 return {
+	escape_key_type = escape_key_type,
 	params_to_class = params_to_class,
-	params_to_cast = params_to_cast
+	params_to_cast = params_to_cast,
 }
