3064e8b06e8a3c04ce0e712344d34ec64cbf80c0
ChillerDragon <chillerdragon@gmail.com>
2026-03-15 16:31:56 +0100

    Unset skin

--- library/player.lua
+++ library/player.lua
@@ -15,3 +15,11 @@ function Player:name() end
 ---@param skin_info SkinInfo # Skin details, all fields are optional. Only updates the provided fields.
 ---@param priority? integer # Lower priorities will not override higher priorities, default value is ddnetpp.skin_priority.HIGH
 function Player:set_skin(skin_info, priority) end
+
+---Remove a previously set skin info.
+---When a skin info is set with set_skin() it will stay set until it is unset using this method.
+---By default it sets and unsets with priority HIGH. If you unset a previously set skin at HIGH
+---it will fallback to priority USER and show again the skin that was originally requested by the client.
+---@param skin_info SkinInfo # Skin details, all fields are optional. Only updates the provided fields.
+---@param priority? integer # Only clears the skin at this priority, default value is ddnetpp.skin_priority.HIGH
+function Player:unset_skin(skin_info, priority) end
