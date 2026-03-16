43d2de051767ca0d71b306c31d3fb132f89dfb0f
ChillerDragon <chillerdragon@gmail.com>
2026-03-16 16:37:37 +0100

    Character die

--- library/character.lua
+++ library/character.lua
@@ -11,3 +11,9 @@ function Character:id() end
 
 ---@return Player player # The player instance
 function Character:player() end
+
+---Be careful to not use the character instance after calling this.
+---It does invalidate it.
+---@param killer? integer # Client id of the killer, by default this is the id of the character that will die
+---@param weapon? integer # Can be any of the ddnetpp.weapon.* constants, by default ddnetpp.weapon.GAME, (not shown in kill feed but thats a bug xd)
+function Character:die(killer, weapon) end
