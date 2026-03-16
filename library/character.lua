a3fbe96cbd9c40298219537f7300bcf402cddebc
ChillerDragon <chillerdragon@gmail.com>
2026-03-16 16:18:14 +0100

    Character id and player

--- library/character.lua
+++ library/character.lua
@@ -3,5 +3,11 @@
 ---@class Character
 local Character = {}
 
----@return Position pos the floating point position of a tee in the world
+---@return Position pos # The floating point position of a tee in the world
 function Character:pos() end
+
+---@return integer client_id # Same as id() on the player instance
+function Character:id() end
+
+---@return Player player # The player instance
+function Character:player() end
